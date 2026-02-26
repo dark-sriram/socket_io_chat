5e1c5348d422fa914c43a1e1acf0e250053e6e77
sri ram <sri812469@gmail.com>
2026-02-26 12:20:22 +0530

    login logout dp

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { login, signup, logout } from "../controllers/auth.controller.js";
+import { login, signup, logout,updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router=express.Router();
 
 router.post("/signup",signup);
 
-router.get("/login",login)
+router.post("/login",login)
 
-router.get("/logout",logout)
+router.post("/logout",logout)
+
+router.put("/update-profile",protectRoute,updateProfile)
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
@@ -9,6 +9,7 @@ const app=express();
 const PORT=ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoute);
 
