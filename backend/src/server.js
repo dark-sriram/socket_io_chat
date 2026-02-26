c90ca025187dbc618e3be5ac9d3f5252c52ef523
sri ram <sri812469@gmail.com>
2026-02-26 19:40:50 +0530

    message

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoute);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT,()=>{
     console.log("app ooduthu in port:" +PORT);
