2717e22cfdd485f750c7985e33149d70027c1755
sri ram <sri812469@gmail.com>
2026-02-27 14:41:00 +0530

    plain chat app ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,18 +6,18 @@ import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 
-const app=express();
+import {app,server} from "./lib/socket.js"
 
 const PORT=ENV.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoute);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log("app ooduthu in port:" +PORT);
     connectDB();
 })
